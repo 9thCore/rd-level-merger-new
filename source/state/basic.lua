989c227a5bfdcbdb9f4795c7afc17fe000d6d09b
9thCore <38700349+9thCore@users.noreply.github.com>
2025-01-06 18:50:01 +0200

    rewrite basic state code
    apparently the oop was bad but whatever

--- source/state/basic.lua
+++ source/state/basic.lua
@@ -1,43 +1,36 @@
-local Slab = require("Slab");
+return function()
+	local obj = {
+		data = {},
+		state = nil
+	};
 
-local StateBasic = {
-	data = {},
-	state = nil
-};
+	function obj:enter(statemachine)
+		-- Callback when the state has just been entered.
+		-- Expected to be initialised here.
+	end
 
-function StateBasic:new(o)
-	o = o or {};
-	setmetatable(o, self);
-	self.__index = self;
-	return o;
+	function obj:update(statemachine, dt)
+		-- Callback on every frame, relating to updating logic.
+		-- Expected to be updated here.
+	end
+
+	function obj:draw(statemachine)
+		-- Callback on every frame, relating to drawing to the screen.
+		-- Expected to be drawn here, but not necessary as Slab can handle it, really.
+	end
+
+	function obj:mousemoved(statemachine, x, y, dx, dy, istouch)
+		-- Callback when the mouse is moved.
+	end
+
+	function obj:mousepressed(statemachine, x, y, button, istouch, presses)
+		-- Callback when the mouse is pressed.
+	end
+
+	function obj:exit(statemachine)
+		-- Callback when the state has been exited.
+		-- Expected to reset stuff here.
+	end
+
+	return obj;
 end
-
-function StateBasic:enter(statemachine)
-	-- Callback when the state has just been entered.
-	-- Expected to be initialised here.
-end
-
-function StateBasic:update(statemachine, dt)
-	-- Callback on every frame, relating to updating logic.
-	-- Expected to be updated here.
-end
-
-function StateBasic:draw(statemachine)
-	-- Callback on every frame, relating to drawing to the screen.
-	-- Expected to be drawn here, but not necessary as Slab can handle it, really.
-end
-
-function StateBasic:mousemoved(statemachine, x, y, dx, dy, istouch)
-	-- Callback when the mouse is moved.
-end
-
-function StateBasic:mousepressed(statemachine, x, y, button, istouch, presses)
-	-- Callback when the mouse is pressed.
-end
-
-function StateBasic:exit(statemachine)
-	-- Callback when the state has been exited.
-	-- Expected to reset stuff here.
-end
-
-return StateBasic;
