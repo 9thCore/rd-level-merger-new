c938e6e94db80ec7d1a59f2628615298097ce27e
9thCore <38700349+9thCore@users.noreply.github.com>
2025-01-06 17:30:12 +0200

    update state base class

--- source/state/basic.lua
+++ source/state/basic.lua
@@ -1,5 +1,6 @@
+local Slab = require("Slab");
+
 local StateBasic = {
-	gui = {},
 	data = {},
 	state = nil
 };
@@ -19,34 +20,24 @@ end
 function StateBasic:update(statemachine, dt)
 	-- Callback on every frame, relating to updating logic.
 	-- Expected to be updated here.
-
-	statemachine.updategui(self.state, dt);
 end
 
 function StateBasic:draw(statemachine)
 	-- Callback on every frame, relating to drawing to the screen.
-	-- Expected to be drawn here.
-
-	statemachine.drawgui(self.state);
+	-- Expected to be drawn here, but not necessary as Slab can handle it, really.
 end
 
 function StateBasic:mousemoved(statemachine, x, y, dx, dy, istouch)
 	-- Callback when the mouse is moved.
-
-	statemachine.mousemovedgui(self.state, x, y, dx, dy, istouch);
 end
 
 function StateBasic:mousepressed(statemachine, x, y, button, istouch, presses)
 	-- Callback when the mouse is pressed.
-
-	statemachine.mousepressedgui(self.state, x, y, button, istouch, presses);
 end
 
 function StateBasic:exit(statemachine)
 	-- Callback when the state has been exited.
 	-- Expected to reset stuff here.
-
-	self.gui = {};
 end
 
 return StateBasic;
